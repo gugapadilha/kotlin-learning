391858109098ff18a02bd327bb265695ace9d17b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-07-23 14:28:44 -0300

    projectKotlin

---
+++ src/main/kotlin/firstCode.kt
@@ -0,0 +1,15 @@
+fun main () {
+    println("Hello, World")
+    println("Hello, Kotlin")
+
+    var myNumber = 10 //Int
+    var myDecimal = 1.0 //Float
+
+    var myString: String //String
+    myString = "sim"
+    myString = "não" //printa o ultimo valor que fora printado
+    println(myString)
+
+    val myAnotherString = "My constant String Value" //Imutable String
+    println(myAnotherString)
+}
