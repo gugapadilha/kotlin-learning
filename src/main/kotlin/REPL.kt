2120631518ef391bab4cd4bceccfd314a963d4cb
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-07-26 16:17:18 -0300

    changesDataType

--- src/main/kotlin/REPL.kt
+++ src/main/kotlin/REPL.kt
@@ -1,3 +1,3 @@
 fun main () { //REPL = READ EVAL PRINT LOOP
-
-}
+              // TESTAR COISAS DIRETAMENTE NO CONSOLE DO INTELLIJ
+}             // TOOLS -> KOTLIN -> KOTLIN REPL (para rodar, digitar CTRL + ENTER)
--- src/main/kotlin/dataTypes.kt
+++ src/main/kotlin/dataTypes.kt
@@ -9,6 +9,33 @@ fun main () {
     var percentage: Double = 90.78
     var gender: Char = 'm'
 
+    //NUMBERS AND BITS DATA TYPES
+
+    val myBite: Byte = 8 //8-bit signed integer
+    val myShort: Short = 16 // 16-bit signed integer
+    val myInt: Int = 32 // 32-bit signer integer
+    val myLong: Long = 64 // 64-bit signed integer
+
+    //decimals
+    val myFloat: Float = 32.00F // 32-bit floating point number
+    var myDouble: Double = 64.00 // 64-bit floating point number
+
+    //println(myBite.toDouble()::class) //class kotlin.Double
+    println(myBite.toLong()::class) //class kotlin.Long
+
+    val bigLong: Long = 1_000_000 //apenas melhor para ler
+    println(bigLong) //ele ignora os _ quanndo imprime na tela
+
+    val newInt = myInt.plus(12)
+    println(newInt)
+
+
+    var firstName = "gustavo" //double quote: String
+    val c: Char = 'x'// 16-bit unicode character.
+    //obrigado usar aspas simples. // single quote: Char
+
+
+
     //Exemplos
     var numeros: Int = 10
     println(numeros + 10)
@@ -20,6 +47,7 @@ fun main () {
         println(false)
     }
 
+
     var fullName: String = "guga Padilha"
     println(fullName)
 
@@ -27,5 +55,6 @@ fun main () {
     println(fullName) //retorna apenas um espaço en branco
 
     println(fullName.isEmpty()) //retona true pois o fullName está sem valor algum
+
 
 }
