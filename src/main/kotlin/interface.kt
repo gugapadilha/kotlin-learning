6a094db63eca9961fd0f6c2cda96fa82f51ba3f5
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-08-11 10:02:19 -0300

    interfacesAtt

--- src/main/kotlin/interface.kt
+++ src/main/kotlin/interface.kt
@@ -3,6 +3,11 @@ fun main () {
     var myBtn = MyButton()
     myBtn.onTouch()
     myBtn.onClick()
+
+    //Interface can contains both Normal Methods and Abstract Methods
+    //but they contains only abstract properties
+    //You cannot create instance of an Interface similar to an Abstract Class
+    //Interface are not class
 }
 
 interface MyInternFaceListener { //you cannot create instance of interface
@@ -12,6 +17,7 @@ interface MyInternFaceListener { //you cannot create instance of interface
      abstract fun onTouch() //Methods in interface are abstract by default
 
     fun onClick() { //Normal methods are public and open by default
+        println("onClick Interface Code: Button Clicked")
 
     }
 }
@@ -25,6 +31,7 @@ class MyButton: MyInternFaceListener {
     }
 
     override fun onClick() { //Optional to override
+        super.onClick()
         println("Button was clicked")
     }
 }
