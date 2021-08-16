dfc2986116f0294b71de690b5361bed8e401a542
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-08-16 09:52:12 -0300

    constructorParametersEnum

--- src/main/kotlin/enum.kt
+++ src/main/kotlin/enum.kt
@@ -4,12 +4,22 @@ fun main () {
 
     val accountTypeFromApi = "gold"
     val accountOnly = AccountType.valueOf(accountTypeFromApi.toUpperCase())
-    println(accountOnly) //its equal to accountType.GOLD, cause both return the same value
+    //println(accountOnly) //its equal to accountType.GOLD, cause both return the same value
+
+    println(AccountType.GOLD)
+    println(AccountType.GOLD.discountPercent)
+    println(AccountType.GOLD.numberOfSubscriptions)
+
+    val value = AccountType.valueOf("Platinium".toUpperCase())
+    println(value)
+    println(value.discountPercent)
+    println(value.numberOfSubscriptions)
+
 }
 
-enum class AccountType {
-    BRONZE,
-    SILVER,
-    GOLD,
-    PLATINIUM
+enum class AccountType(var discountPercent: Int,var numberOfSubscriptions: Int) {
+    BRONZE(10, 20),
+    SILVER(15, 10),
+    GOLD(20, 5),
+    PLATINIUM(25, 2)
 }
