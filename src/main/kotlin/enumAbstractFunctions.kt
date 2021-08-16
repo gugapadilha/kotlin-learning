3da22675f2f081a786068b5cb4d16fee87acdb21
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-08-16 10:40:24 -0300

    enumAbstractClass

---
+++ src/main/kotlin/enumAbstractFunctions.kt
@@ -0,0 +1,28 @@
+fun main () {
+    val fromApi = "BronZE"
+    val accountTypes = AccountTypes.valueOf(fromApi.toUpperCase())
+    println(fromApi.toUpperCase())
+    println(accountTypes.calculatedDiscountPercent())
+
+}
+
+enum class AccountTypes() {
+    BRONZE {
+        override fun calculatedDiscountPercent() = 5 //instead to open {} and write return 5, i can do that
+    },
+    SILVER{
+        override fun calculatedDiscountPercent() = 10
+    },
+    GOLD{
+        override fun calculatedDiscountPercent(): Int {
+            return 15 //i can do this way too, have no problem
+        }
+    },
+    PLATNIUM{
+        override fun calculatedDiscountPercent()= 20
+    };
+
+    abstract fun calculatedDiscountPercent(): Int
+
+
+}
