2682440df772d162f3a4262c7f028af8ee9b2bce
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-08-18 10:40:49 -0300

    protectedModifier&Methods

--- src/main/kotlin/protectedModifier.kt
+++ src/main/kotlin/protectedModifier.kt
@@ -2,30 +2,35 @@ fun main () {
     val p = Pessoals("guga", 19) //cannot set the favorite food in Pessoals cause is protected
     val c = Chef("bob", 30, "ribs")
 
-    p.showsMyFavoriteFood() // i can just show the favorite food
+    //p.showsMyFavoriteFood() // i can just show the favorite food
 
-    c.showsMyFavoriteFood()
+    //c.showsMyFavoriteFood()
     c.setMyFavoriteFood("Onions")
-    c.showsMyFavoriteFood()
+    //c.showsMyFavoriteFood()
 }
 
 open class Pessoals(var name: String, var age: Int) {
-    protected var favoriteFood: String = "unknown"
-
+    
     fun info () {
         println("this person's name is $name and they are $age yo")
     }
+}
 
-    fun showsMyFavoriteFood() {
+open class Chef(name: String, age: Int, var favoriteFood: String): Pessoals(name, age){
+
+    protected fun showsMyFavoriteFood() {
         println(favoriteFood)
+    }
+
+    fun setMyFavoriteFood(food: String){
+        favoriteFood = food
+        showsMyFavoriteFood()
     }
 }
 
-class Chef(name: String, age: Int, favFood: String): Pessoals(name, age){
-    init {
-        favoriteFood = favFood
-    }
-    fun setMyFavoriteFood(food: String){
-        favoriteFood = food
+class SousChef(name: String, age: Int, var favFood: String): Chef(name, age, favFood){
+
+    init { //just can call in init, even if is a protected.I cannos print that on my main
+        showsMyFavoriteFood()
     }
 }
