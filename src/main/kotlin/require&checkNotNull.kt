c7b45c235e963d5e93d4f13c6cf9be74c7678797
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-08-31 14:08:49 -0300

    check&filterNotNull

--- src/main/kotlin/require&checkNotNull.kt
+++ src/main/kotlin/require&checkNotNull.kt
@@ -13,7 +13,26 @@ fun main () {
 
     //RequireNotNull - forcing something thats is nullable to be not null
 
-   val person2: PersonNames? = null //PersonName("Guga")
+    val person2: PersonNames? = PersonNames("guga") //PersonNames("Guga")
 
     println(requireNotNull(person2, {"Person shouldnt be null"}).name) //shows this message in the console
+
+
+    //checkNotNull
+
+    val person3: PersonNames? = PersonNames("Guga") //PersonName("Guga")
+
+    if (checkNotNull(person3).name == "Guga") {
+        println("hi guga!")
+    } else {
+        println("hi")
+    }
+
+
+    //filterNotNull
+
+    val names = listOf<String?>("guga", "jake", "john", null, "sophia", "alexa", null, "isaias")
+
+    val namesNotNull = names.filterNotNull()
+    println(namesNotNull)
 }
