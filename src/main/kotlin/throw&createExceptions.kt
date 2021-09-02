061f0c193d9bc6eead25cb10727831ed8a3beaa6
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-09-02 15:09:11 -0300

    throwsExceptions

--- src/main/kotlin/throw&createExceptions.kt
+++ src/main/kotlin/throw&createExceptions.kt
@@ -10,14 +10,35 @@ fun main () {
 //    }
 
 
-    //CREATE A CUSTOM EXCEPTION
-    val p2 = NomeIdade("resters", 19)
+//    //CREATE A CUSTOM EXCEPTION
+//    val p2 = NomeIdade("resters", 19)
+//
+//    if(p2.age < 21) {
+//        throw InvalidAgeException(p2.age, "User is not old enough")
+//    }
 
-    if(p2.age < 21) {
-        throw InvalidAgeException(p2.age, "User is not old enough")
-    }
     
+    //TRY CATCH STATEMENT
+    val p3 = NomeIdade("gugaran", 16)
 
+    try {
+        checkAge(p3)
+        println("Done")
+        println("Done")
+        println("Done")
+        println("Done")
+        println("Done")
+    } catch(ex: Exception) {
+        println("Caught the exception") //the application will not CRASH.
+        //if something goes wron with apllication, the program will print that
+    }
+}
+
+private fun checkAge(p3: NomeIdade) {
+    if(p3.age < 18) {
+        throw Exception("FOO")
+    }
+    println("after catch that")
 }
 
 data class NomeIdade(var name: String, var age: Int)
