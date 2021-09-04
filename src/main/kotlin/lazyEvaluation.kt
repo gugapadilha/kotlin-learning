cdc14d79a80d5af2dd9ed2bf61560cf8745f2cac
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-09-04 10:49:36 -0300

    lazyBlockEvaluation

--- src/main/kotlin/lazyEvaluation.kt
+++ src/main/kotlin/lazyEvaluation.kt
@@ -1,6 +1,9 @@
 import java.lang.Thread.sleep
+import kotlin.random.Random
 
 fun main () {
+    //LAZY OPERATOR
+
     val name: String by lazy {
         println("Computed") //printed just once
         println("foobar")   //printed just once, only lazy is printed many times.
@@ -11,4 +14,22 @@ fun main () {
     println(name)
     println(name)
     println(name)
+
+
+    //LAZY INITIALIZER BLOCK
+    
+    val resul:Lazy<Int> = lazy {someExpensiveOperation()}
+
+    println(resul)
+    println("is initialized: ${resul.isInitialized()}")
+    println(resul.value) //the first time this values is computed inside  on someExpensiveOperation
+    println(resul.value) // in subsequentes times we gonna see the same value
+    println(resul.value)
+
+}
+
+fun someExpensiveOperation(): Int {
+    println("computed")
+    sleep(1000)
+    return Random(System.currentTimeMillis()).nextInt() //gering a random value
 }
