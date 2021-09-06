9534ef0fd6c5258559ebf93f0f5280f80c085b81
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-09-06 14:47:10 -0300

    lambaExpression

--- src/main/kotlin/lambdaExpressions.kt
+++ src/main/kotlin/lambdaExpressions.kt
@@ -1,8 +1,20 @@
 fun main () {
     // val lambdaName: (InputType) -> ReturnType = { arguments:InputType -> body}
 
-    //greeter ("guga")
-    //"guga padilha"
-    val greeter: (String) -> String = { name: String -> "Hello $name" }
-    println(greeter("guga"))
+    //greeter (" Hello guga")
+    /*val greeter: (String) -> String = { name: String-> "Hello $name" }
+    val greeting = greeter("guga")
+    println(greeting) //printing Hello guga
+    */
+
+
+    //"Hello GUGA padilha"
+    val greeterLambda: (String, String) -> String = { firstName: String, lastName: String ->
+        val modifiedFirstName: String = firstName.toUpperCase()
+        val modifiedLastName: String = lastName.toLowerCase()
+        "Hello $modifiedFirstName $modifiedLastName"
+    }
+
+    val greetingLambda: String = greeterLambda("guga", "padilha")
+    println(greetingLambda)
 }
