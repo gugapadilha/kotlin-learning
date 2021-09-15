532d8ac8cda4ce7e64669789a61c25c88e00c773
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-09-15 15:11:20 -0300

    lambdaExpressionWhen

--- src/main/kotlin/lambdaExpression2.kt
+++ src/main/kotlin/lambdaExpression2.kt
@@ -2,6 +2,17 @@ fun main () {
 
     val program = Programs()
     program.reverseAndDisplay("hello", {it.reversed()})
+
+
+    var person = Pessouan()
+
+    person.apply {
+        name = "guga"
+        age = 19
+    }.startRun()
+
+    println(person.name)
+    println(person.age)
 }
 
 class Programs {
@@ -9,5 +20,15 @@ class Programs {
     fun reverseAndDisplay(str: String, myFunc: (String) -> String) { //High Level Function with lambda as parameter
         var result = myFunc(str) //it.reversed() =====> str.reversed =====> "hello".reversed() = "olleh"
         println(result)
+    }
+}
+
+class Pessouan {
+
+    var name: String = ""
+    var age: Int = 0
+
+    fun startRun() {
+        println("now i'm ready to run")
     }
 }
