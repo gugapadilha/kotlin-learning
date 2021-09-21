f0dae4a93d3b2981eed7aa110ec03fad2074f5d7
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-09-21 10:30:57 -0300

    collectionsAtt

---
+++ src/main/kotlin/collections.kt
@@ -0,0 +1,19 @@
+fun main () {
+
+    //Elements:  32   0    0    54   0
+    //index:     0    1    2    3    4
+
+    var myArray = Array<Int>(5) { 0 } //mutable but fixed size
+    myArray[0] = 32
+    myArray[3] = 54
+
+    for (element in myArray) { //using individual element (objects)
+        println(element)
+    }
+
+    println()
+
+    for (index in 0..myArray.size -1 ) { //i can use index in 0..4 as well, is the same thing
+        println(myArray[index]) //printing all the elements inside de array
+    }
+}
