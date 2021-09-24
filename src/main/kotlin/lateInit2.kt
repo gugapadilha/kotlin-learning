8e8648f81015092cfce627ca064ef3f35cc60df8
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-09-24 11:28:00 -0300

    lateInit2Att

--- src/main/kotlin/lateInit2.kt
+++ src/main/kotlin/lateInit2.kt
@@ -1,11 +1,30 @@
 fun main () {
 
-    //var country = Country()
+    var country = Country()
     //country.name = "Brazil"
-    println(country.name)ffs
+    //println(country.name)
+    country.setup()
+
+
+    var otherCountry = Country()
+    otherCountry.newCountry()
 }
 
 class Country {
 
     lateinit var name: String
+
+    fun setup() {
+        name = "Brazil"
+        println("The name of the country is $name")
+    }
+
+    fun newCountry() {
+        name = "USA"
+        println("the name of the new country is $name")
+    }
 }
+
+//lateinit only use only with mutable data type [ var ]
+//lateinit used only with non-nullable data type
+//lateinit values must be initialized before you use it
